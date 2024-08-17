d94aeabdd5085a88eda09811607f8628c93d4609
Ahmed Ihab <ahmedinouh@gmail.com>
2024-08-17 09:01:33 +0300

    will be continued

---
+++ login_registeration/login_registeration.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string enter_login_name;
+    cout<<"Enter name: ";
+    cin>>enter_login_name;
+    int password;
+    cout<<"enter password";
+    cin>>password;
+
+
+}
